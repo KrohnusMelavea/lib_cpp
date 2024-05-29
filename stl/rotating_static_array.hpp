09ce967bc3a903d4fd99f8bf4be98f67a9549f33
Daniel de Waal <danieldewaal02@gmail.com>
2024-05-29 16:34:32 +0200

    added stl::rotating_static_array boilerplate

--- stl/rotating_static_array.hpp
+++ stl/rotating_static_array.hpp
@@ -13,22 +13,29 @@ namespace stl {
   constexpr void push(T const& value) noexcept { 
    if (m_capacity == array_size) [[likely]] {
     m_data[((m_index = (m_index + 1) % array_size) + array_size - 1) % array_size] = value;
-   } else {
+   } else [[unlikely]] {
     m_data[m_capacity++] = value;
    }
   }
-  inline constexpr auto& operator[] (std::size_t const i) noexcept { return m_data[(m_index + i) % array_size]; }
-  inline constexpr auto const& operator[] (std::size_t const i) const noexcept { return m_data[(m_index + i) % array_size]; }
+  [[nodiscard]] inline constexpr auto& operator[] (std::size_t const i) noexcept { return m_data[(m_index + i) % array_size]; }
+  [[nodiscard]] inline constexpr auto const& operator[] (std::size_t const i) const noexcept { return m_data[(m_index + i) % array_size]; }
 
-  inline constexpr auto begin() noexcept { return rotating_array_iterator{ m_data, m_index, m_capacity }; }
-  inline constexpr auto end() noexcept { return iterator_sentinel{}; }
-  inline constexpr auto begin() const noexcept { return rotating_array_const_iterator{ m_data, m_index, m_capacity }; }
-  inline constexpr auto end() const noexcept { return iterator_sentinel{}; }
-  inline constexpr auto cbegin() const noexcept { return rotating_array_const_iterator{ m_data, m_index, m_capacity }; }
-  inline constexpr auto cend() const noexcept { return iterator_sentinel{}; }
+  [[nodiscard]] inline constexpr auto begin() noexcept { return rotating_array_iterator{ m_data, m_index, m_capacity }; }
+  [[nodiscard]] inline constexpr auto end() noexcept { return iterator_sentinel{}; }
+  [[nodiscard]] inline constexpr auto begin() const noexcept { return rotating_array_const_iterator{ m_data, m_index, m_capacity }; }
+  [[nodiscard]] inline constexpr auto end() const noexcept { return iterator_sentinel{}; }
+  [[nodiscard]] inline constexpr auto cbegin() const noexcept { return rotating_array_const_iterator{ m_data, m_index, m_capacity }; }
+  [[nodiscard]] inline constexpr auto cend() const noexcept { return iterator_sentinel{}; }
 
-  inline constexpr auto size() const noexcept { return array_size; }
-  inline constexpr auto capacity() const noexcept { return m_capacity; };
+  [[nodiscard]] inline constexpr auto& front() noexcept { return m_data[m_index]; }
+  [[nodiscard]] inline constexpr auto const& front() const noexcept { return m_data[m_index]; }
+  [[nodiscard]] inline constexpr auto& back() noexcept { return m_data[(m_index + m_capacity - 1) % m_capacity]; }
+  [[nodiscard]] inline constexpr auto const& back() const noexcept { return m_data[(m_index + m_capacity - 1) % m_capacity]; } 
+
+  [[nodiscard]] inline constexpr auto data() noexcept { return m_data; }
+  [[nodiscard]] inline constexpr auto const data() const noexcept { return m_data; }
+  [[nodiscard]] inline constexpr auto size() const noexcept { return array_size; }
+  [[nodiscard]] inline constexpr auto capacity() const noexcept { return m_capacity; };
 
  private:
   T m_data[array_size];
