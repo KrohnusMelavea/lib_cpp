be0b19ebe072c4412c29f632aab7b36e2fed50a3
Daniel de Waal <danieldewaal02@gmail.com>
2024-05-29 12:48:10 +0200

    Fleshed out Callback for timing::scoped_timer

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -3,13 +3,5 @@
 #include <spdlog/spdlog.h>
 
 int main() {
- {
-  timing::cycle_timer timer(timing::timing_archive::global_callback);
-  for (u32 i = 0; i < 10; ++i) {
-   for (u32 j = 0; j < 1000000000; ++j) {
-    
-   }
-   SPDLOG_INFO("{}", timer.lap().count());
-  }
- }
+ 
 }
