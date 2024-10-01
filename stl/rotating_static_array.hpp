23801a66cc2ee54fb09aef3a6cf5d32f40def625
Daniel de Waal <danieldewaal02@gmail.com>
2024-10-01 19:29:05 +0200

    added emplace, made push perfect forwarding, and 'this->ified' everything

--- stl/rotating_static_array.hpp
+++ stl/rotating_static_array.hpp
@@ -15,57 +15,65 @@ namespace stl {
 
   constexpr rotating_static_array(rotating_static_array const& rotating_static_array) noexcept : m_index{rotating_static_array.m_index}, m_capacity{rotating_static_array.m_capacity} {
    for (std::size_t i = 0; i < array_size; ++i) {
-    m_data[i] = rotating_static_array.m_data[i];
+    this->m_data[i] = rotating_static_array.m_data[i];
    }
   }
   constexpr rotating_static_array(rotating_static_array&& rotating_static_array) noexcept : m_index{rotating_static_array.m_index}, m_capacity{rotating_static_array.m_capacity} {
    for (std::size_t i = 0; i < array_size; ++i) {
-    m_data[i] = std::move(rotating_static_array.m_data[i]);
+    this->m_data[i] = std::move(rotating_static_array.m_data[i]);
    }
   }
 
-  constexpr auto& operator= (rotating_static_array const& rotating_static_array) noexcept {
+  constexpr auto& operator=(rotating_static_array const& rotating_static_array) noexcept {
    for (std::size_t i = 0; i < array_size; ++i) {
-    m_data[i] = rotating_static_array.m_data[i];
+    this->m_data[i] = rotating_static_array.m_data[i];
    }
    return *this;
   }
-  constexpr auto& operator= (rotating_static_array&& rotating_static_array) noexcept {
+  constexpr auto& operator=(rotating_static_array&& rotating_static_array) noexcept {
    for (std::size_t i = 0; i < array_size; ++i) {
-    m_data[i] = std::move(rotating_static_array.m_data[i]);
+    this->m_data[i] = std::move(rotating_static_array.m_data[i]);
    }
    return *this;
   }
 
-  constexpr void push(T const& value) noexcept { 
-   if (m_capacity == array_size) [[likely]] {
-    m_data[((m_index = (m_index + 1) % array_size) + array_size - 1) % array_size] = value;
+  constexpr void push(T&& value) noexcept {
+   if (this->m_capacity == array_size) [[likely]] {
+    this->m_data[((this->m_index = (this->m_index + 1) % array_size) + array_size - 1) % array_size] = std::forward<T>(value);
    } else [[unlikely]] {
-    m_data[m_capacity++] = value;
+    this->m_data[this->m_capacity++] = std::forward<T>(value);
    }
   }
-  [[nodiscard]] inline constexpr auto& operator[] (std::size_t const i) noexcept { return m_data[(m_index + i) % array_size]; }
-  [[nodiscard]] inline constexpr auto const& operator[] (std::size_t const i) const noexcept { return m_data[(m_index + i) % array_size]; }
+  constexpr T& emplace(auto&&... args) noexcept {
+   if (this->m_capacity == array_size) [[likely]] {
+    this->m_data[((this->m_index = (this->m_index + 1) % array_size) + array_size - 1) % array_size] = T{std::forward<decltype(args)>(args)...};
+   } else [[unlikely]] {
+    this->m_data[this->m_capacity++] = T{std::forward<decltype(args)>(args)...};
+   }
+   return this->m_data[(this->m_index + this->m_capacity - 1) % this->m_capacity];
+  }
+  [[nodiscard]] inline constexpr auto& operator[] (std::size_t const i) noexcept { return this->m_data[(this->m_index + i) % array_size]; }
+  [[nodiscard]] inline constexpr auto const& operator[] (std::size_t const i) const noexcept { return this->m_data[(this->m_index + i) % array_size]; }
 
-  [[nodiscard]] inline constexpr auto begin() noexcept { return rotating_array_iterator{ m_data, m_index, m_capacity }; }
+  [[nodiscard]] inline constexpr auto begin() noexcept { return rotating_array_iterator{ this->m_data, this->m_index, this->m_capacity }; }
   [[nodiscard]] inline constexpr auto end() noexcept { return iterator_sentinel{}; }
-  [[nodiscard]] inline constexpr auto begin() const noexcept { return rotating_array_const_iterator{ m_data, m_index, m_capacity }; }
+  [[nodiscard]] inline constexpr auto begin() const noexcept { return rotating_array_const_iterator{ this->m_data, this->m_index, this->m_capacity }; }
   [[nodiscard]] inline constexpr auto end() const noexcept { return iterator_sentinel{}; }
-  [[nodiscard]] inline constexpr auto cbegin() const noexcept { return rotating_array_const_iterator{ m_data, m_index, m_capacity }; }
+  [[nodiscard]] inline constexpr auto cbegin() const noexcept { return rotating_array_const_iterator{ this->m_data, this->m_index, this->m_capacity }; }
   [[nodiscard]] inline constexpr auto cend() const noexcept { return iterator_sentinel{}; }
 
-  [[nodiscard]] inline constexpr auto& front() noexcept { return m_data[m_index]; }
-  [[nodiscard]] inline constexpr auto const& front() const noexcept { return m_data[m_index]; }
-  [[nodiscard]] inline constexpr auto& back() noexcept { return m_data[(m_index + m_capacity - 1) % m_capacity]; }
-  [[nodiscard]] inline constexpr auto const& back() const noexcept { return m_data[(m_index + m_capacity - 1) % m_capacity]; } 
+  [[nodiscard]] inline constexpr auto& front() noexcept { return this->m_data[this->m_index]; }
+  [[nodiscard]] inline constexpr auto const& front() const noexcept { return this->m_data[this->m_index]; }
+  [[nodiscard]] inline constexpr auto& back() noexcept { return this->m_data[(this->m_index + this->m_capacity - 1) % this->m_capacity]; }
+  [[nodiscard]] inline constexpr auto const& back() const noexcept { return this->m_data[(this->m_index + this->m_capacity - 1) % this->m_capacity]; } 
 
-  [[nodiscard]] inline constexpr auto full() const noexcept { return m_capacity == array_size; }
-  [[nodiscard]] inline constexpr auto empty() const noexcept { return m_capacity == 0; }
+  [[nodiscard]] inline constexpr auto full() const noexcept { return this->m_capacity == array_size; }
+  [[nodiscard]] inline constexpr auto empty() const noexcept { return this->m_capacity == 0; }
 
-  [[nodiscard]] inline constexpr auto data() noexcept { return m_data; }
-  [[nodiscard]] inline constexpr auto const data() const noexcept { return m_data; }
+  [[nodiscard]] inline constexpr auto data() noexcept { return this->m_data; }
+  [[nodiscard]] inline constexpr auto const data() const noexcept { return this->m_data; }
   [[nodiscard]] inline constexpr auto size() const noexcept { return array_size; }
-  [[nodiscard]] inline constexpr auto capacity() const noexcept { return m_capacity; };
+  [[nodiscard]] inline constexpr auto capacity() const noexcept { return this->m_capacity; };
 
  private:
   T m_data[array_size];
